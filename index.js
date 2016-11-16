777452e27e64ae4448231c0083c6f29b5d3461b6
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-11-16 14:40:02 -0700

    Completed mini project

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+
+
+// Create a massive instance
+
+var connectionString = "postgres://localhost/sandbox";
+
+var massiveInstance = massive.connectSync({connectionString : connectionString})
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+app.set('db', massiveInstance);
+
+// var db = app.get('db');
+
+var controller = require('./controller.js');
+
+controller.getPlanes();
+
+// db.new_plane(function(err, planes){
+//     console.log(err, "plane added")
+// });
+
+// db.get_planes(function(err, planes){
+//     console.log(err, planes)
+// })
 
 app.listen('3000', function(){
   console.log("Successfully listening on : 3000")	
